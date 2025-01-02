dd1623f67e8f749886ea9540c9a635feadaedbd8
Damianos Gkouzkouris <damgouzkouris@yahoo.com>
2025-01-02 11:35:38 +0000

    Update build_python-ldap.cmd
    Changing the build_python-ldap.cmd to pull from main directly

--- build_python-ldap.cmd
+++ build_python-ldap.cmd
@@ -2,9 +2,9 @@
 @echo on
 setlocal
 
-:: set PYTHONLDAP_VER=python-ldap-3.4.4
+:: set PYTHONLDAP_VER=main
 
-curl -L -o %PYTHONLDAP_VER%.tar.gz https://github.com/python-ldap/python-ldap/archive/refs/tags/%PYTHONLDAP_VER%.tar.gz
+curl -L -o %PYTHONLDAP_VER%.tar.gz https://github.com/python-ldap/python-ldap/archive/refs/head/%PYTHONLDAP_VER%.tar.gz
 if errorlevel 1 exit /B 1
 
 tar -xf %PYTHONLDAP_VER%.tar.gz --strip-components=1
